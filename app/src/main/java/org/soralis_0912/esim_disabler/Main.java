089c4138a108fa43901b9763f08b69e7810932dd
soralis <soralis.nem@gmail.com>
2024-01-21 02:20:49 +0900

    add A13 support

--- app/src/main/java/org/soralis_0912/esim_disabler/Main.java
+++ app/src/main/java/org/soralis_0912/esim_disabler/Main.java
@@ -4,6 +4,7 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.XposedBridge;
 
 public class Main implements IXposedHookLoadPackage {
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
@@ -11,11 +12,29 @@ public class Main implements IXposedHookLoadPackage {
             return;
         }
 
-        XposedHelpers.findAndHookMethod("com.android.phone.MiuiEsimManagerBase", lpparam.classLoader, "isRemoveEsimSwitch", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                param.setResult(false);
-            }
-        });
+        // for A14
+        try {
+            XposedHelpers.findAndHookMethod("com.android.phone.MiuiEsimManagerBase", lpparam.classLoader, "isRemoveEsimSwitch", new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    param.setResult(false);
+                }
+            });
+        } catch (Throwable t) {
+            XposedBridge.log("Failed to hook com.android.phone.MiuiEsimManagerBase method");
+        }
+
+        // for A13
+        try {
+            XposedHelpers.findAndHookMethod("com.android.phone.MiuiEsimManager", lpparam.classLoader, "isRemoveEsimSwitch", new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    param.setResult(false);
+                }
+            });
+        } catch (Throwable t) {
+            XposedBridge.log("Failed to hook com.android.phone.MiuiEsimManager method");
+        }
+        
     }
 }
